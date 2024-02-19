314608101cd1981133a2a04e7a307e0f6552b390
Extremepaul <jeanp86@hotmail.com>
2024-02-19 13:15:56 -0400

    testing working and comments in English

--- game.test.js
+++ game.test.js
@@ -1,21 +1,18 @@
-// game.test.js
-
 const axios = require('axios');
 
 describe('fetchPuzzleData', () => {
   it('should receive response from the API', async () => {
     try {
-      // Hacer una solicitud HTTP a la URL
+      // Make an HTTP request to the URL
       const response = await axios.get('https://prog2700.onrender.com/threeinarow/sample');
 
-      // Verificar si la respuesta contiene un JSON válido
+      // Check if the response contains valid JSON
       expect(response.data).toBeDefined();
       expect(typeof response.data).toBe('object');
 
-      // Si la prueba llega a este punto, significa que la URL responde con un JSON válido
-    } catch (error) {
-      // Si se produce un error, hacemos que la prueba falle explícitamente
-      expect(error).toBeUndefined(); // Esto asegura que la prueba falle
+      } catch (error) {
+      // If an error occurs make the test fail
+      expect(error).toBeUndefined(); 
     }
   });
 });
